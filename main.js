2db010f2441657dfd19193b31a679a2306621126
Aaron Anderson <aaronlynanderson@gmail.com>
2017-08-03 15:10:02 -0400

    Added shuffle functions and classList.toggle('flipped') to all dives

--- main.js
+++ main.js
@@ -1,11 +1,34 @@
-let cardName = document.getElementsByClassName('card');
+let cardsTypes = ['a','b','c','d','e','f','g','i'];
+console.log(cardsTypes);
+let cardsDeck = cardsTypes.concat.apply(cardsTypes,cardsTypes);
+console.log(cardsDeck);
 
-console.log(cardName);
+// Here I shall teach main.js to shuffle cards //
+function shuffle(thisArray){
+  for (i=0; i<100; i++){
+    popped = thisArray.pop();
+    randomPosition= Math.floor(Math.random()*cardsDeck.length);
+    thisArray.splice(randomPosition,0,popped);
+  };
+}
+// Woohoo it works!!!! //
+// Shuffle function has been created //
+shuffledDeck = shuffle(cardsDeck);
+//console.log(shuffledDeck);
+let cardIds = []
 
-cardName[0].addEventListener('mouseover', flipCard);
-
-function flipCard(){
-cardName[0].classList.toggle("flipped");
+// Append Divs operation below //
+gameBoard = document.querySelector('.game_board');
+console.log(gameBoard);
+//for on cardsDeck array here//
+for (i=0; i<cardsDeck.length; i++){
+  card = document.createElement('div');
+  console.log(card);
+  card.setAttribute('class','card ' + i);
+  card.innerHTML = `<p>${cardsDeck[i]}<p>`;
+  gameBoard.appendChild(card);
+  card.setAttribute('onclick', "card.classList.toggle('flipped')");
+  cardIds.push(i);
 }
 
-console.log(cardName);
+console.log(cardIds);
